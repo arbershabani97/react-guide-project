e4ca98270548e77c5d4ee4156bfcba9c788bfbce
Arber Shabani <arber.shabani@hotmail.com>
2020-05-11 04:43:34 +0200

    Remove Bootstrap Styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Router, Route, Switch} from "react-router-dom";
 import history from "./history";
 import Projects from "./components/projects/Projects";
+import "./App.scss";
 
 const App = () => {
 	return (
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import {Provider} from "react-redux";
 
 import App from "./App";
 import store from "./store";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
 	<Provider store={store}>
